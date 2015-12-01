e2073b5d11166f1f7af0d7bfe24c1e3ef6dce3ce
Vladimir Agafonkin <agafonkin@gmail.com>
2015-12-01 19:29:57 +0200

    generalize more

--- index.js
+++ index.js
@@ -18,66 +18,63 @@ function network(input, output, rows, hiddenSize, alpha) {
     var syn1d = matrix2d(hiddenSize, outputSize);
     var syn1t = syn1.transpose(1, 0);
 
-    var l0 = x;
-    var l0t = l0.transpose(1, 0);
+    var l0 = {};
+    l0.activation = x;
+    l0.transposed = x.transpose(1, 0);
 
-    var l1 = matrix2d(rows, hiddenSize);
-    var l1e = matrix2d(rows, hiddenSize);
-    var l1d = matrix2d(rows, hiddenSize);
-    var l1t = l1.transpose(1, 0);
-
-    var l2 = matrix2d(rows, outputSize);
-    var l2e = matrix2d(rows, outputSize);
-    var l2d = matrix2d(rows, outputSize);
+    var l1 = createLayer(rows, hiddenSize);
+    var l2 = createLayer(rows, outputSize);
 
     for (var i = 0; i <= 60000; i++) {
         feedforward(l1, l0, syn0);
         feedforward(l2, l1, syn1);
 
-        backpropagate(l2d, l2, sub(l2e, l2, y));
-        backpropagate(l1d, l1, dotProduct(l1e, l2d, syn1t));
+        sub(l2.error, l2.activation, y);
+        updateDelta(l2);
 
-        updateWeights(syn1, dotProduct(syn1d, l1t, l2d), alpha);
-        updateWeights(syn0, dotProduct(syn0d, l0t, l1d), alpha);
+        dotProduct(l1.error, l2.delta, syn1t);
+        updateDelta(l1);
 
-        if (i % 5000 === 0) console.log('err ' + i + ': ' + mean(l2e));
+        updateWeights(syn1, syn1d, l1, l2, alpha);
+        updateWeights(syn0, syn0d, l0, l1, alpha);
+
+        if (i % 5000 === 0) console.log('err ' + i + ': ' + mean(l2.error ));
     }
+}
+
+function updateWeights(synapse, deltas, layer1, layer2, alpha) {
+    dotProduct(deltas, layer1.transposed, layer2.delta);
+    for (var i = 0; i < deltas.data.length; i++) synapse.data[i] -= alpha * deltas.data[i];
+}
+
+function updateDelta(a) {
+    for (var i = 0, values = a.activation.data; i < values.length; i++) {
+        a.delta.data[i] = values[i] * (1 - values[i]) * a.error.data[i];
+    }
+}
+
+function feedforward(output, input, synapse) {
+    dotProduct(output.activation, input.activation, synapse);
+    for (var i = 0, data = output.activation.data; i < data.length; i++) data[i] = 1 / (1 + Math.exp(-data[i]));
+}
+
+function createLayer(rows, cols) {
+    var obj = {};
+    var shape = [rows, cols];
+    obj.activation = ndarray(new Float32Array(rows * cols), shape);
+    obj.error = ndarray(new Float32Array(rows * cols), shape);
+    obj.delta = ndarray(new Float32Array(rows * cols), shape);
+    obj.transposed = obj.activation.transpose(1, 0);
+    return obj;
 }
 
 function sub(out, a, b) {
     for (var i = 0; i < a.data.length; i++) out.data[i] = a.data[i] - b.data[i];
-    return out;
-}
-
-function updateWeights(synapse, deltas, alpha) {
-    for (var i = 0; i < deltas.data.length; i++) synapse.data[i] -= alpha * deltas.data[i];
-}
-
-function backpropagate(out, layer, error) {
-    for (var i = 0; i < layer.data.length; i++) {
-        var v = layer.data[i];
-        out.data[i] = v * (1 - v) * error.data[i];
-    }
-    return out;
-}
-
-function feedforward(out, layer, synapse) {
-    return sigmoid(out, dotProduct(out, layer, synapse));
 }
 
 function matrix2d(rows, cols, data) {
     return ndarray(new Float32Array(data || (rows * cols)), [rows, cols]);
 }
-
-function sigmoid(out, a) {
-    for (var i = 0; i < a.data.length; i++) out.data[i] = 1 / (1 + Math.exp(-a.data[i]));
-    return out;
-};
-
-function sigmoidDeriv(out, a) {
-    for (var i = 0; i < a.data.length; i++) out.data[i] = a.data[i] * (1 - a.data[i]);
-    return out;
-};
 
 function dotProduct(out, a, b) {
     for (var i = 0; i < a.shape[0]; i++) {
